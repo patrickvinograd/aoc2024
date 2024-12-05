522067ffaf911d247311e3211419e8cf1d91ac96
Patrick <vinograd@gmail.com>
2024-12-04 21:52:53 -0800

    Day 5

---
+++ day5a.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parseRules(r []string) map[int][]int {
+	result := make(map[int][]int)
+	for _, v := range r {
+		vals := strings.Split(v, "|")
+		a, _ := strconv.Atoi(vals[0])
+		b, _ := strconv.Atoi(vals[1])
+		result[a] = append(result[a], b)
+	}
+	return result
+}
+
+func parseUpdates(u []string) [][]int {
+	result := make([][]int, 0)
+	for _, v := range u {
+		vals := strings.Split(v, ",")
+		update := make([]int, len(vals))
+		for i, u := range vals {
+			update[i], _ = strconv.Atoi(u)
+		}
+		result = append(result, update)
+	}
+	return result
+}
+
+func valid(update []int, rules map[int][]int) bool {
+	for i := 0; i < len(update); i++ {
+		for j := i; j < len(update); j++ {
+			if i == j {
+				continue
+			}
+			if slices.Contains(rules[update[j]], update[i]) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func fixUpdate(update []int, rules map[int][]int) []int {
+	fixed := make([]int, len(update))
+	_ = copy(fixed, update)
+	slices.SortFunc(fixed, func(a, b int) int {
+		if slices.Contains(rules[a], b) {
+			return -1
+		} else if slices.Contains(rules[b], a) {
+			return 1
+		} else {
+			return 0
+		}
+	})
+	return fixed
+}
+
+func processUpdates(updates [][]int, rules map[int][]int) int {
+	total := 0
+	for _, update := range updates {
+		if !valid(update, rules) {
+			fixed := fixUpdate(update, rules)
+			// fmt.Println(update, fixed)
+			total += fixed[len(update)/2]
+		}
+	}
+	return total
+}
+
+func main() {
+
+	var r []string
+	var u []string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var gap = false
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			gap = true
+			continue
+		}
+		if gap == false {
+			r = append(r, line)
+		} else {
+			u = append(u, line)
+		}
+	}
+	rules := parseRules(r)
+	updates := parseUpdates(u)
+	fmt.Println(processUpdates(updates, rules))
+}
